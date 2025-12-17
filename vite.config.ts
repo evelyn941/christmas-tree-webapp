8c0baa5e85e0b90615800ab9b7d9ddaa6ac9a89f
Evelyn <hyq941@gmail.com>
2025-12-16 22:37:51 -0500

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,10 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      base: "/christmas-tree-webapp/",
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // IMPORTANT: Replace 'REPO_NAME' below with your actual GitHub repository name.
+  // Example: If your repo URL is github.com/username/christmas-card, this should be '/christmas-card/'
+  base: '/christmas-tree-webapp/', 
+})
