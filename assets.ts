2843f45c68388864dd06c58a462b9e28309f0e37
Evelyn <hyq941@gmail.com>
2025-12-16 22:07:28 -0500

    Configure assets and GitHub Pages deployment

--- assets.ts
+++ assets.ts
@@ -9,18 +9,34 @@
  * 3. Uncomment the import lines below and add them to the arrays.
  */
 
-// --- 1. IMPORT BACKGROUND ---
-// import myBackground from './images/bg.jpg'; 
+ // --- 1. IMPORT BACKGROUND ---
+ // Background image import commented out until you add ./images/bg.png to the project.
+import myBackground from './images/bg.png'; 
 
 // --- 2. IMPORT PHOTOS ---
-// import p1 from './images/1.jpg';
-// import p2 from './images/2.jpg';
-// import p3 from './images/3.jpg';
-// import p4 from './images/4.jpg';
-// import p5 from './images/5.jpg';
+import p1 from './images/1.jpg';
+import p2 from './images/2.jpg';
+import p3 from './images/3.jpg';
+import p4 from './images/4.jpg';
+import p5 from './images/5.jpg';
+import p6 from './images/6.jpg';
+import p7 from './images/7.jpg';
+import p8 from './images/8.jpg';
+import p9 from './images/9.png';
+import p10 from './images/10.jpg';
+import p11 from './images/11.jpg';
+import p12 from './images/12.jpg';
+import p13 from './images/13.jpg';
+import p14 from './images/14.jpg';
+import p15 from './images/15.jpg';
+import p16 from './images/16.jpg';
+import p17 from './images/17.jpg';
+import p18 from './images/18.jpg';
+import p19 from './images/19.jpg';
+import p20 from './images/20.jpg';
 
 // --- 3. IMPORT MUSIC ---
-// import myMusic from './images/song.mp3'; // Or ./audio/song.mp3 if you made an audio folder
+import myMusic from './images/song.mp3'; // Or ./audio/song.mp3 if you made an audio folder
 
 
 /**
@@ -28,7 +44,7 @@
  * Set this to `myBackground` (or whatever you named the import above).
  * If null, it uses the default dark green 3D scene.
  */
-export const USER_BACKGROUND: string | null = null; 
+export const USER_BACKGROUND: string | null = myBackground; 
 
 /**
  * EXPORT PHOTOS
@@ -38,7 +54,8 @@ export const USER_BACKGROUND: string | null = null;
  * If this array is empty [], the app will load the random internet placeholders.
  */
 export const USER_PHOTOS: string[] = [
-    // p1, p2, p3...
+p1, p2, p3, p4, p5, p6, p7, p8, p9, p10,
+p11, p12, p13, p14, p15, p16, p17, p18, p19, p20
 ];
 
 /**
@@ -46,4 +63,4 @@ export const USER_PHOTOS: string[] = [
  * Set this to `myMusic` (or whatever you named the import above).
  * Supported formats: .mp3, .wav, .ogg, .m4a
  */
-export const USER_MUSIC: string | null = null; 
+export const USER_MUSIC: string | null = myMusic; 
